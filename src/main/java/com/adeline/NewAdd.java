1e2cdf94779b181dcc3053d2a8b9025e7f3fb1be
Adeline Pan <adeline.pan@thomsonreuters.com>
2016-08-12 11:43:48 +0800

    svn test branch

--- src/main/java/com/adeline/NewAdd.java
+++ src/main/java/com/adeline/NewAdd.java
@@ -4,5 +4,6 @@ public class NewAdd {
 	public static void main(String[] args){
 		System.out.println("add new file");
 		System.out.println("branch test");
+		System.out.println("svn branch test");
 	}
 }
